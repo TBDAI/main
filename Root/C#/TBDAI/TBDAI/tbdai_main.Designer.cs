d0e9758210b9b4fd6e6b4be08599a4ba9ed4749d
watchnite23 <bozhidarbotev683@gmail.com>
2023-02-02 20:28:06 +0100

    Reads and sends input output zu datain
    In class I have to make it start a test.py file and make the input go in there and the test.py to return something that will also be returned in the c# TBDAI app

--- Root/C#/TBDAI/TBDAI/tbdai_main.Designer.cs
+++ Root/C#/TBDAI/TBDAI/tbdai_main.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.inputTxtBox = new System.Windows.Forms.RichTextBox();
             this.outputTxtBox = new System.Windows.Forms.RichTextBox();
             this.sendBtn = new System.Windows.Forms.Button();
+            this.thinkLbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // inputTxtBox
@@ -38,11 +41,11 @@
             this.inputTxtBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(69)))), ((int)(((byte)(73)))));
             this.inputTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.inputTxtBox.ForeColor = System.Drawing.SystemColors.Window;
-            this.inputTxtBox.Location = new System.Drawing.Point(34, 34);
-            this.inputTxtBox.Margin = new System.Windows.Forms.Padding(26, 25, 26, 25);
+            this.inputTxtBox.Location = new System.Drawing.Point(23, 22);
+            this.inputTxtBox.Margin = new System.Windows.Forms.Padding(17, 16, 17, 16);
             this.inputTxtBox.MaxLength = 244;
             this.inputTxtBox.Name = "inputTxtBox";
-            this.inputTxtBox.Size = new System.Drawing.Size(560, 97);
+            this.inputTxtBox.Size = new System.Drawing.Size(373, 63);
             this.inputTxtBox.TabIndex = 0;
             this.inputTxtBox.Text = "";
             // 
@@ -51,11 +54,11 @@
             this.outputTxtBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(69)))), ((int)(((byte)(73)))));
             this.outputTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.outputTxtBox.ForeColor = System.Drawing.SystemColors.Window;
-            this.outputTxtBox.Location = new System.Drawing.Point(34, 208);
-            this.outputTxtBox.Margin = new System.Windows.Forms.Padding(26, 25, 26, 25);
+            this.outputTxtBox.Location = new System.Drawing.Point(23, 135);
+            this.outputTxtBox.Margin = new System.Windows.Forms.Padding(17, 16, 17, 16);
             this.outputTxtBox.Name = "outputTxtBox";
             this.outputTxtBox.ReadOnly = true;
-            this.outputTxtBox.Size = new System.Drawing.Size(560, 257);
+            this.outputTxtBox.Size = new System.Drawing.Size(373, 167);
             this.outputTxtBox.TabIndex = 20;
             this.outputTxtBox.Text = "";
             // 
@@ -65,25 +68,43 @@
             this.sendBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.sendBtn.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.sendBtn.ForeColor = System.Drawing.SystemColors.Control;
-            this.sendBtn.Location = new System.Drawing.Point(623, 34);
+            this.sendBtn.Location = new System.Drawing.Point(415, 22);
+            this.sendBtn.Margin = new System.Windows.Forms.Padding(2);
             this.sendBtn.Name = "sendBtn";
-            this.sendBtn.Size = new System.Drawing.Size(104, 97);
+            this.sendBtn.Size = new System.Drawing.Size(69, 63);
             this.sendBtn.TabIndex = 1;
             this.sendBtn.Text = "send";
             this.sendBtn.UseVisualStyleBackColor = false;
             this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
             // 
+            // thinkLbl
+            // 
+            this.thinkLbl.Font = new System.Drawing.Font("Verdana", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thinkLbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.thinkLbl.Location = new System.Drawing.Point(20, 111);
+            this.thinkLbl.Name = "thinkLbl";
+            this.thinkLbl.Size = new System.Drawing.Size(92, 16);
+            this.thinkLbl.TabIndex = 21;
+            this.thinkLbl.Text = "Thinking..";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // tbdForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(57)))), ((int)(((byte)(62)))));
-            this.ClientSize = new System.Drawing.Size(978, 498);
+            this.ClientSize = new System.Drawing.Size(652, 324);
+            this.Controls.Add(this.thinkLbl);
             this.Controls.Add(this.sendBtn);
             this.Controls.Add(this.outputTxtBox);
             this.Controls.Add(this.inputTxtBox);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "tbdForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -95,6 +116,8 @@
         private System.Windows.Forms.RichTextBox inputTxtBox;
         private System.Windows.Forms.RichTextBox outputTxtBox;
         private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label thinkLbl;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Root/C#/TBDAI/TBDAI/tbdai_main.cs
+++ Root/C#/TBDAI/TBDAI/tbdai_main.cs
@@ -9,30 +9,67 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.IO;
-
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 
 namespace TBDAI
 {
     public partial class tbdForm : Form
     {
+
         public tbdForm()
         {
             InitializeComponent();
+            thinkLbl.Visible = false;
         }
 
         private void sendBtn_Click(object sender, EventArgs e)
         {
-            run_cmd();
+            thinkLbl.Visible = true;
+            timer1.Start();
+            
+            
         }
         private void run_cmd()
         {
-            
-            string text = File.ReadAllText("C:/Users/Realschule Boxberg 1/Desktop/Projekt/main/Root/Python/outputpy.txt");
-            outputTxtBox.Text = text;
+            string currentDirectory = Environment.CurrentDirectory;
+            string filePath = Path.Combine(currentDirectory, @"..\..\..\..\..\Python\outputpy.txt");
+            string text = File.ReadAllText(filePath);
+            outputTxtBox.Text = text; 
+            }
+        private void inputsend()
+        {
+            // Get the text from the textbox
+            string textboxInput = inputTxtBox.Text.ToString();
+
+            // Set the path for the text file
+            string path = @"..\..\..\..\..\Python\inputcs.txt";
+
+            // Check if the file exists
+            if (File.Exists(path))
+            {
+                // If the file exists, delete it
+                File.Delete(path);
+            }
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(textboxInput);
+            }
+
+            run_cmd();
+
 
         }
-    
-}
+
+
+            private void timer1_Tick(object sender, EventArgs e)
+        {
+            thinkLbl.Visible = false;
+            inputsend();
+            timer1.Stop();
+        }
+    }
 
     
 }
